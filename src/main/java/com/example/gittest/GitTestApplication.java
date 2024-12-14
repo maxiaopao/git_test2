8137fc16159eb82c9670f7f183e12e3b9bfe5bd7
mafei <>
2024-12-14 20:34:16 +0800

    init

---
+++ src/main/java/com/example/gittest/GitTestApplication.java
@@ -0,0 +1,14 @@
+package com.example.gittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTestApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		SpringApplication.run(GitTestApplication.class, args);
+	}
+
+}
